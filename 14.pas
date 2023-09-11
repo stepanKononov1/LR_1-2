9213fdb5776c126a3773e92ab2b335a3847a1962
stepanKononov1 <94189109+stepanKononov1@users.noreply.github.com>
2023-09-11 10:21:12 +0300

    Add files via upload

--- 14.pas
+++ 14.pas
@@ -4,25 +4,25 @@ begin
   writeln('Введите координату ладьи (стобец)');
   while true do
   begin
-    read(x1); if (x1>8) or (x1<0) then write('Такой координаты не существует, введите ещё раз')
+    read(x1); if (x1>8) or (x1<1) then write('Такой координаты не существует, введите ещё раз')
     else break;
   end;
   writeln('Введите координату ладьи (строку)');
   while true do
   begin
-    read(y1); if (y1>8) or (y1<0) then write('Такой координаты не существует, введите ещё раз')
+    read(y1); if (y1>8) or (y1<1) then write('Такой координаты не существует, введите ещё раз')
     else break;
   end;
   writeln('Введите координаты вражеской фигуры (стобец)');
   while true do
   begin
-    read(x2); if (x2>8) or (x2<0) then write('Такой координаты не существует, введите ещё раз')
+    read(x2); if (x2>8) or (x2<1) then write('Такой координаты не существует, введите ещё раз')
     else break;
   end;
   writeln('Введите координаты вражеской фигуры (строку)');
   while true do
   begin
-    read(y2); if (y2>8) or (y2<0) then write('Такой координаты не существует, введите ещё раз')
+    read(y2); if (y2>8) or (y2<1) then write('Такой координаты не существует, введите ещё раз')
     else break;
   end;
   if (y1=y2) or (x1=x2) then writeln('Ладья бьёт вражескую фигуру')
