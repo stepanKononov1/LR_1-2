f8b03051d0c20e3ac17be6126816888816ba069a
stepanKononov1 <94189109+stepanKononov1@users.noreply.github.com>
2023-09-11 11:16:31 +0300

    Update 5.pas

--- 5.pas
+++ 5.pas
@@ -3,6 +3,6 @@ var x: integer;
 begin
   write('Введите не дробное число: ');
   read(x);
-  x:= (x div 10) * 10 + (x div 10)
+  x:= (x div 10) * 10 + (x div 10);
   write('Прибавлен нуль перед младшим разрядом: ', x*10);
 end.
